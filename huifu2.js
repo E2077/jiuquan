4166e9fb6b1f30d4bded3e937bd352f91534e0fd
E2077 <88936201+E2077@users.noreply.github.com>
2023-04-15 21:41:58 +0800

    Update huifu2.js

--- huifu2.js
+++ huifu2.js
@@ -15,19 +15,19 @@ const https = require('https');
 const url = `https://api.jiucaishuo.com/v2/image/comments`;
 const method = `POST`;
 const headers = {
-    'Accept': `*/*`,
-    'Accept-Encoding': `gzip;q=1.0, compress;q=0.5`,
-    'Connection': `keep-alive`,
-    'Content-Type': `application/x-www-form-urlencoded; charset=utf-8`,
-    'Host': `api.jiucaishuo.com`,
-    'User-Agent': `JiuQuan/229 (jiuquaner.app.chen; build:229; iOS 12.5.5) Alamofire/229`,
-    'Accept-Language': `zh-Hans-CN;q=1.0, en-CN;q=0.9, zh-Hant-HK;q=0.8, zh-Hant-CN;q=0.7`
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip;q=1.0, compress;q=0.5',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+    'Host': 'api.jiucaishuo.com',
+    'User-Agent': 'JiuQuan/227 (jiuquaner.app.chen; build:227; iOS 16.2.0) Alamofire/227',
+    'Accept-Language': 'zh-Hans-CN;q=1.0'
 };
 
 function sendRequest() {
     // 生成5位随机数
     const randomNumber = Math.floor(10000 + Math.random() * 90000);
-   const body = `abiokytke=68&act_time=1681564589947&authtoken=%2BWQC415cWnnFJ1O9JJysmVAu9C4QQsp2&bd24y6421f=8f&bd4uy742=1&bgd7h8tyu54=3b&bgiuytkw=4f&bioduytlw=3&bvytikwqjk=3b&com=1&commend_uid=519244&comment_type=1&fjlkatj=f2b&ghtoiutkmlg=ad0&h13ey474=e2d&h67456y=5a7&hy5641d321t=f1&ibvytiqjek=f7&iogojti=f&is_app=1&jnhf8u5231=4f&kf54ge7=d&lksytkjh=a7da&n3bf4uj7y7=7&nbf4uj7y432=68&nd354uy4752=2&ngd4uy551=a7&ngd4yut78=d0&nkjhrew=1&no_push=1&original_id=297027&quikgdky=a9&relay_id=297027&sbnoywr=b8&tbvdiuytk=5&text_id=297027&tiklsktr4=a&tirgkjfs=ea&type=app_ios&u54rg5d=f2&version=2.3.3&word=${randomNumber}&y654b5fs3tr=a&yi854tew=e2&yt447e13f=f`;
+   const body = `abiokytke=87&act_time=1681558563528&authtoken=P6WQLI4aOvxZpXmEQ6%2Bu9EETbzroVPOG&bd24y6421f=f3&bd4uy742=2&bgd7h8tyu54=d2&bgiuytkw=53&bioduytlw=1&bvytikwqjk=d2&com=1&commend_uid=510585&comment_type=1&fjlkatj=1b2&ghtoiutkmlg=911&h13ey474=2bd&h67456y=7ed&hy5641d321t=32&ibvytiqjek=40&iogojti=3&is_app=1&jnhf8u5231=53&kf54ge7=d&lksytkjh=ed50&n3bf4uj7y7=d&nbf4uj7y432=87&nd354uy4752=b&ngd4uy551=ed&ngd4yut78=11&nkjhrew=2&no_push=1&original_id=297026&quikgdky=5c&relay_id=297026&sbnoywr=7f&tbvdiuytk=7&text_id=297026&tiklsktr4=3&tirgkjfs=53&type=app_ios&u54rg5d=1b&version=2.3.6&word=${randomNumber}&y654b5fs3tr=9&yi854tew=2b&yt447e13f=d`;
 
     const options = {
         hostname: 'api.jiucaishuo.com',
